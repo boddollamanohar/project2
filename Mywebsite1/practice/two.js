71372da18ff5e2e6012bc3c30d3718d3dec5c4ee
unknown <iambmanohar@gmail.com>
2023-01-05 13:08:26 +0530

    data

---
+++ Mywebsite1/practice/two.js
@@ -0,0 +1,6 @@
+/* let state = { id : 101,productname: "Iphone",price :45000, color1:"black", color2:"blue"}
+let = {id, price,color1} = state
+console.log(id);
+console.log(price);
+console.log(color1); */
+let state = { id : 101,productname: "Iphone",price :45000, color1:"black", color2:"blue"}
